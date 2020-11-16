b0d8ad2a2c1f30458cc8d6c12dabbff28be5ae89
Shalaw Fatah <shalaw.fatah@gmail.com>
2020-11-16 12:05:59 -0800

    test of access

--- src/store.ts
+++ src/store.ts
@@ -7,7 +7,7 @@ The complete set of contributors may be found at http://polymer.github.io/CONTRI
 Code distributed by Google as part of the polymer project is also
 subject to an additional IP rights grant found at http://polymer.github.io/PATENTS.txt
 */
-
+// Here is a comment by Shalaw
 declare global {
     interface Window {
       process?: Object;
